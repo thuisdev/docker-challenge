49154d5f86290ac7edb08f6a88fe7d1d7508c305
Adrian <adrian.thuis04@gmail.com>
2026-05-20 19:00:15 +0200

    add docker-compose with mongodb

--- docker-challenge/app.js
+++ docker-challenge/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const PORT = 3000;
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/myapp';
+
+mongoose.connect(MONGO_URL)
+  .then(() => console.log('MongoDB verbunden'))
+  .catch((err) => console.log('MongoDB Fehler:', err));
 
 app.get('/', (req, res) => {
-  res.send('Hello from Docker!');
+  res.send('Hello from Docker mit MongoDB!');
 });
 
 app.listen(PORT, () => {
